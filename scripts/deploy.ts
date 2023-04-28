85fd5fa723b82244c896cadfda027ef6d72732e0
David Ho <davidho0403@gmail.com>
2023-04-29 00:42:38 +0800

    Reset code

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,12 +4,12 @@ async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
-    const lockedAmount = ethers.utils.parseEther("0.001");
+  const lockedAmount = ethers.utils.parseEther("0.001");
 
   const Lock = await ethers.getContractFactory("Lock");
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed()
+  await lock.deployed();
 
   console.log(
     `Lock with ${ethers.utils.formatEther(
