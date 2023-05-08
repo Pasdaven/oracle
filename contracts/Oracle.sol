03058ad6e88a561c7b5130efe83061e6f92584bc
ridemountainpig <lin0905580802@gmail.com>
2023-05-08 20:19:41 +0800

    feat: add processRequest and getResponse function
    processRequest function will get the request from DApp and check the data is invalid or not, if valid it will check data type to run numeric or string contract.
    
    getResponses function is use to test processRequest work successful or not.

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "./Numeric.sol";
+import "./String.sol";
+
 contract Oracle {
     struct Data {
         string dataType;
@@ -12,7 +15,26 @@ contract Oracle {
         string message;
     }
 
-    function checkDataType(Data memory _data) pure external returns (Response memory) {
+    Response[] public responses;
+
+    function processRequest(Data memory requestData) public {
+        Response memory dataTypeCheck = checkDataType(requestData);
+        Response memory response;
+        if (keccak256(bytes(dataTypeCheck.status)) == keccak256(bytes("invalid"))) {
+            response = dataTypeCheck;
+        }
+
+        if (keccak256(bytes(requestData.dataType)) == keccak256(bytes("Numeric"))) {
+            Numeric numericContract = new Numeric();
+            response = numericContract.numericFunction(requestData.content);
+        } else if (keccak256(bytes(requestData.dataType)) == keccak256(bytes("String"))) {
+            String stringContract = new String();
+            response = stringContract.stringFunction(requestData.content);
+        }
+        responses.push(response);
+    }
+
+    function checkDataType(Data memory _data) public pure returns (Response memory) {
         bytes memory numericType = bytes("Numeric");
         bytes memory stringType = bytes("String");
 
@@ -23,5 +45,10 @@ contract Oracle {
         }
 
         return Response("valid", "Valid data type");
+    }
+
+    function getResponses() public view returns (Response memory) {
+        uint256 length = responses.length;
+        return responses[length - 1];
     }
 }
