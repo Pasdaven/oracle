7c42269b90fceb048bff05823e372cf777e9ceff
pascal <a306090603@gmail.com>
2023-05-18 18:05:48 +0800

    temp

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,28 +1,56 @@
 import { ethers } from 'hardhat';
-// import { Contract } from 'hardhat/internal/hardhat-network/stack-traces/model';
+import { Contract, ContractFactory, ethers as e } from 'ethers';
+
+interface contractParams {
+    abi: e.utils.BytesLike;
+    address: string;
+}
 
 async function main() {
 
     const numericIntegration = await deployContract('NumericIntegration');
+
     const stringIntegration = await deployContract('StringIntegration');
 
     const numericProcess = await deployContract('NumericProcess', numericIntegration);
-    const stringProcess = await deployContract('StringProcess', stringIntegration);
+    console.log(numericProcess.abi);
+    console.log(numericProcess.address);
 
-    const oracle = await deployContract('Oracle', numericProcess, stringProcess);
+    // const stringProcess = await deployContract('StringProcess', stringIntegration);
+
+    // const oracle = await deployContract('Oracle', numericProcess, stringProcess);
 
 }
 
-async function deployContract(contractName: string, ...constructorContract: Contract[]): Promise<Contract> {
-    const ContractFactory = await ethers.getContractFactory(contractName);
-    let contract;
-    if (!constructorContract.length) {
-        contract = await ContractFactory.deploy();
+async function deployContract(contractName: string, ...contractParamsArr: contractParams[]): Promise<contractParams> {
+    const ContractFactory: ContractFactory = await ethers.getContractFactory(contractName);
+    let contract: Contract;
+    
+    if (contractParamsArr.length) {
+        const deployParams: any[] = [];
+        for (const contractParams of contractParamsArr) {
+            deployParams.push(contractParams.abi, contractParams.address);
+        }
+        contract = await ContractFactory.deploy(...deployParams);
     } else {
         contract = await ContractFactory.deploy();
     }
     await contract.deployed();
-    return contract;
+    return { abi: convertABItoBytes(ContractFactory.interface.fragments), address: contract.address };
+}
+
+function convertABItoBytes(abi: readonly e.utils.Fragment[]): e.utils.BytesLike {
+    const abiBytes = ethers.utils.defaultAbiCoder.encode(['string'], [JSON.stringify(abi)]);
+    return abiBytes;
+}
+
+function convertToSolidityBytes(fragments: e.utils.Fragment[]): string {
+    // const abiEncodedData = e.utils.defaultAbiCoder.encode(fragments.map(fragment => fragment.format));
+    const abiEncodedData = e.utils.defaultAbiCoder.encode(fragments.map(fragment => fragment.type), fragments.map(() => 0));
+
+    const solidityBytes = e.utils.hexlify(e.utils.concat([e.utils.hexZeroPad(e.utils.hexStripZeros(abiEncodedData), 32), e.utils.hexValue(fragments.length)]));
+
+    return solidityBytes;
 }
 
 
