c7e2c567ddad82de58527567f26f4ff247aeb17e
shuhuai Chang <95022717+pascal910107@users.noreply.github.com>
2023-05-25 12:16:45 +0800

    feat: Make deployment scripts to bind multiple smart contracts (#40)
    * update deploy.ts content
    
    * create NumericIntegration contract
    
    * create StringIntegration contract
    
    * update param _abi type
    
    * fix: compile error
    
    * temp
    
    * temp
    
    * change binding method
    
    * deploy and bind multiple contracts
    
    * add NumericIntegration test function
    
    * Rename numericIntegration.sol to NumericIntegration.sol
    
    * Rename stringIntegration.sol to StringIntegration.sol
    
    * update: format style
    
    ---------
    
    Co-authored-by: Yen Cheng <92412722+ridemountainpig@users.noreply.github.com>
    Co-authored-by: ridemountainpig <lin0905580802@gmail.com>

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+interface NumericProcess {
+    function createEvent(uint256 _questionId, string memory _question) external;
+    function answerQuestion(uint256 _questionId, uint256 _answer) external;
+    function getAnswerByAnswerer(uint256 _questionId, address answerer) external view returns (uint256);
+    function getAllAnswerers(uint256 _questionId) external view returns (address[] memory);
+    function callNumericIntegrationContract() external view;
+}
+
+interface StringProcess {
+    function createEvent(uint256 _questionId, string memory _question) external;
+    function answerQuestion(uint256 _questionId, string memory _answer) external;
+    function getAnswerByAnswerer(uint256 _questionId, address answerer) external view returns (string memory);
+    function getAllAnswerers(uint256 _questionId) external view returns (address[] memory);
+    function callStringIntegrationContract() external view;
+}
+
 contract Oracle {
     struct Data {
         string dataType;
@@ -17,6 +33,14 @@ contract Oracle {
     Response[] public responses;
     mapping(uint256 => address) public requestIndexToAddress;
     uint256 public requestIndexLength;
+
+    NumericProcess private numericProcess;
+    StringProcess private stringProcess;
+
+    constructor(address _numericProcessAddr, address _stringProcessAddr) {
+        numericProcess = NumericProcess(_numericProcessAddr);
+        stringProcess = StringProcess(_stringProcessAddr);
+    }
 
     function processRequest(Data memory requestData) public {
         requestIndexToAddress[requestIndexLength] = requestData.callBackAddress;
