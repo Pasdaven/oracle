d45fe7803ed7eb398f924dc1aa0a1eab86a0f3b4
TTigger <sg0421051@gmail.com>
2023-05-25 19:21:23 +0800

    rename Static to AddressRecord

--- contracts/AddressRecord.sol
+++ contracts/AddressRecord.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Static {
+contract AddressRecord {
     address[] public latestAddressRecord;
     function getLatestDeployAddress(address latestDeployAddress) external returns (string memory) {
         latestAddressRecord.push(latestDeployAddress);
