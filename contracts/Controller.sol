828538247ae5c5f5d13174914d3c7f3c4ede24f9
TTigger <sg0421051@gmail.com>
2023-06-18 22:32:24 +0800

    use import file instead of interface

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -1,22 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-interface NumericProcess {
-    function answerQuestion(uint256 questionId, uint256 answerContent, address walletAddress) external returns (string memory);
-}
-
-interface StringProcess {
-    function answerQuestion(uint256 questionId, string memory answerContent, address walletAddress) external returns (string memory);
-}
-
-interface Authentication {
-    function register(address _walletAddress) external;
-}
-
-interface ProvideEvent {
-    function getNumericQuestions(address walletAddress) external view returns (uint256[] memory, string[] memory);
-    function getStringQuestions(address walletAddress) external view returns (uint256[] memory, string[] memory);
-}
+import "./NumericProcess.sol";
+import "./StringProcess.sol";
+import "./Authentication.sol";
+import "./ProvideEvent.sol";
 
 contract Controller {
     NumericProcess public numericContract;
