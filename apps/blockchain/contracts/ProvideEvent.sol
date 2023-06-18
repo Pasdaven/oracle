4e76a9cabc9052164bd0c3b56e3af440b07d3172
pascal <a306090603@gmail.com>
2023-06-18 20:08:56 +0800

    feat: add ProvideEvent contract function

--- apps/blockchain/contracts/ProvideEvent.sol
+++ apps/blockchain/contracts/ProvideEvent.sol
@@ -1,39 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-interface NumericProcess {
-    struct Question {
-        uint256 questionId;
-        string question;
-        mapping(address => uint256) answers;
-        address[] answerers;
-        bool isExists;
-    }
-    function createEvent(uint256 _questionId, string memory _question) external;
-    function answerQuestion(uint256 _questionId, uint256 _answer, address _walletAddress) external;
-    function getAnswerByAnswerer(uint256 _questionId, address answerer) external view returns (uint256);
-    function getAllAnswerers(uint256 _questionId) external view returns (address[] memory);
-    function callNumericIntegrationContract() external view;
-    function getAllQuestions() external view returns (mapping(uint256 => Question) memory);
-}
-interface StringProcess {
-    struct Question {
-        uint256 questionId;
-        string question;
-        mapping(address => string) answers;
-        address[] answerers;
-        bool isExists;
-    }
-    function createEvent(uint256 _questionId, string memory _question) external;
-    function answerQuestion(uint256 _questionId, string memory _answer, address _walletAddress) external;
-    function getAnswerByAnswerer(uint256 _questionId, address answerer) external view returns (string memory);
-    function getAllAnswerers(uint256 _questionId) external view returns (address[] memory);
-    function callStringIntegrationContract() external view;
-    function getAllQuestions() external view returns (mapping(uint256 => Question) memory);
-}
+import "./NumericProcess.sol";
+import "./StringProcess.sol";
 
 contract ProvideEvent {
 
-    // function 
+    NumericProcess private numericProcess;
+    StringProcess private stringProcess;
 
+    constructor(address _numericProcessAddr, address _stringProcessAddr) {
+        numericProcess = NumericProcess(_numericProcessAddr);
+        stringProcess = StringProcess(_stringProcessAddr);
+    }
+
+    function getNumericQuestions() external view returns (uint256[] memory, string[] memory) {
+        return numericProcess.getQuestions();
+    }
+
+    function getStringQuestions() external view returns (uint256[] memory, string[] memory) {
+        return stringProcess.getQuestions();
+    }
 }
