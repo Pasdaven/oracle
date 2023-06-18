9ba3b9973fa2c69ef3e28eb06e364e1ef3cbd0c1
ridemountainpig <lin0905580802@gmail.com>
2023-06-18 23:48:47 +0800

    Update page.tsx

--- apps/frontend/app/login/page.tsx
+++ apps/frontend/app/login/page.tsx
@@ -6,6 +6,12 @@ import { useState } from 'react';
 export default function WalletPage() {
     const [account, setAccount] = useState('');
     const [error, setError] = useState('');
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const handleSubmit = async () => {
+        setIsSubmit(true);
+        await connectWallet(setError, setAccount);
+    };
 
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
@@ -13,22 +19,23 @@ export default function WalletPage() {
                 <h1 className="text-4xl font-bold">Metamask authentication</h1>
             </div>
             <div className="flex justify-center w-full py-5">
-                {account ? (
+                {account && (
                     <div>
                         <p className="px-4 py-2 rounded-md bg-purple-600 cursor-pointer hover:bg-purple-500 text-xl font-semibold duration-100 text-white">
                             {account}
                         </p>
                     </div>
-                ) : (
+                )}
+                {!isSubmit && (
                     <button
                         className="px-4 py-2 rounded-md bg-purple-600 cursor-pointer hover:bg-purple-500 text-xl font-semibold duration-100 text-white"
-                        onClick={() => connectWallet(setError, setAccount)}
+                        onClick={handleSubmit}
                     >
                         Connect
                     </button>
                 )}
                 {error && (
-                    <p className={`error shadow-border`}>{`Error: ${error}`}</p>
+                    <p className="px-4 py-2 rounded-md bg-purple-600 cursor-pointer hover:bg-purple-500 text-xl font-semibold duration-100 text-white">{`Error: ${error}`}</p>
                 )}
             </div>
         </main>
