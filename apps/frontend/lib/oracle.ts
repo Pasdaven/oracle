f4851087a871174ae27c4f53ec8df5f17703ba0e
David Ho <davidho0403@gmail.com>
2023-06-19 23:35:26 +0800

    feat: set up the provider and signer in oracle lib

--- apps/frontend/lib/oracle.ts
+++ apps/frontend/lib/oracle.ts
@@ -1,23 +1,44 @@
-import { ethers } from 'ethers';
+import { Contract, ethers } from 'ethers';
 import controllerAbi from '../../blockchain/artifacts/contracts/Controller.sol/Controller.json';
 
-const contractAddress = '';
-const contractABI = controllerAbi.abi;
-const provider = new ethers.JsonRpcProvider();
+const getProvider = () => {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    return provider;
+};
 
-const contract = new ethers.Contract(contractAddress, contractABI, provider);
+const getSigner = async () => {
+    const signer = await getProvider().getSigner();
+    return signer;
+};
+
+const getContract = async () => {
+    const signer = await getSigner();
+    const contractAddress = '0x959922bE3CAee4b8Cd9a407cc3ac1C251C2007B1';
+    const contractABI = controllerAbi.abi;
+    const contract = new Contract(contractAddress, contractABI, signer);
+    return contract;
+};
+
+export const foo = async () => {
+    const contract = await getContract();
+    const request = await contract.foo();
+    return request;
+};
 
 export const authenticate = async (walletAddress: string) => {
+    const contract = await getContract();
     const request = await contract.auth(walletAddress);
     return request;
 };
 
 export const getNumericEvent = async (walletAddress: string) => {
+    const contract = await getContract();
     const request = await contract.getNumericEvent(walletAddress);
     return request;
 };
 
 export const getStringEvent = async (walletAddress: string) => {
+    const contract = await getContract();
     const request = await contract.getStringEvent(walletAddress);
     return request;
 };
@@ -27,6 +48,7 @@ export const answerNumericQuestion = async (
     answerContent: number,
     walletAddress: string
 ) => {
+    const contract = await getContract();
     const request = await contract.answerNumericQuestion(
         questionId,
         answerContent,
@@ -40,6 +62,7 @@ export const answerStringQuestion = async (
     answerContent: string,
     walletAddress: string
 ) => {
+    const contract = await getContract();
     const request = await contract.answerStringQuestion(
         questionId,
         answerContent,
