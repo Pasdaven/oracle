074f1814e424a2717b860b78a431b939699760e6
David Ho <davidho0403@gmail.com>
2023-09-10 17:56:08 +0800

    feat: update hardhat

--- apps/sample-dapp/blockchain/hardhat.config.ts
+++ apps/sample-dapp/blockchain/hardhat.config.ts
@@ -1,16 +1,8 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
-import { HttpNetworkAccountsUserConfig } from 'hardhat/types';
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.18',
-  networks: {
-    ganache: {
-      url: 'http://localhost:7545',
-      chainId: 1337,
-      accounts: process.env.PRIVATE_KEY as HttpNetworkAccountsUserConfig,
-    },
-  },
+  solidity: '0.8.9',
 };
 
 export default config;
