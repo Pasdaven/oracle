e7d62374690247c8173c3215714cdd98e17aa98c
Ho Yi Wei <75478661+davidho0403@users.noreply.github.com>
2023-09-10 18:18:22 +0800

    Migrate to pnpm (#208)
    * feat: migrate to pnpm
    
    * style: prettier formatting
    
    * feat: change to pnpm in ci
    
    * feat: add pnpm setup in ci
    
    * feat: add pnpm version in package.json
    
    * feat: add build dependencies
    
    * feat: update pnpm lock
    
    * fix: update oracle abi import path
    
    * feat: change ci node ver to 16
    
    * feat: add build dependencies in turbo
    
    * feat: update pnpm lock
    
    * feat: update hardhat

--- apps/blockchain/test/Authentication.ts
+++ apps/blockchain/test/Authentication.ts
@@ -3,62 +3,57 @@ import { expect } from 'chai';
 import { Contract } from 'ethers';
 
 describe('Authentication', function () {
-    let authentication: Contract;
-    beforeEach(async () => {
-        const Authentication = await ethers.getContractFactory(
-            'Authentication'
-        );
-        authentication = await Authentication.deploy();
-        await authentication.deployed();
+  let authentication: Contract;
+  beforeEach(async () => {
+    const Authentication = await ethers.getContractFactory('Authentication');
+    authentication = await Authentication.deploy();
+    await authentication.deployed();
+  });
+
+  describe('register', function () {
+    this.beforeEach(async () => {
+      const registeredAddress = '0x1234567890123456789012345678901234567890';
+      await authentication.register(registeredAddress);
     });
 
-    describe('register', function () {
-        this.beforeEach(async () => {
-            const registeredAddress =
-                '0x1234567890123456789012345678901234567890';
-            await authentication.register(registeredAddress);
-        });
-
-        it('should return true for a valid address', async () => {
-            const address = '0x0987654321098765432109876543210987654321';
-            await expect(authentication.register(address))
-                .to.emit(authentication, 'Register')
-                .withArgs(true);
-        });
-
-        it('should return false for an invalid address', async () => {
-            const address = '0x1234567890123456789012345678901234567890';
-            await expect(authentication.register(address))
-                .to.emit(authentication, 'Register')
-                .withArgs(false);
-        });
+    it('should return true for a valid address', async () => {
+      const address = '0x0987654321098765432109876543210987654321';
+      await expect(authentication.register(address))
+        .to.emit(authentication, 'Register')
+        .withArgs(true);
     });
 
-    describe('getUsers', function () {
-        it('should return an array of addresses', async () => {
-            const result = await authentication.getUsers();
-            expect(result).to.be.an('array');
-        });
+    it('should return false for an invalid address', async () => {
+      const address = '0x1234567890123456789012345678901234567890';
+      await expect(authentication.register(address))
+        .to.emit(authentication, 'Register')
+        .withArgs(false);
+    });
+  });
+
+  describe('getUsers', function () {
+    it('should return an array of addresses', async () => {
+      const result = await authentication.getUsers();
+      expect(result).to.be.an('array');
+    });
+  });
+
+  describe('verifyUserIsRegistered', function () {
+    it('should return true for a registered address', async () => {
+      const registeredAddress = '0x1234567890123456789012345678901234567890';
+      await authentication.register(registeredAddress);
+      const result = await authentication.verifyUserIsRegistered(
+        registeredAddress
+      );
+      expect(result).to.equal(true);
     });
 
-    describe('verifyUserIsRegistered', function () {
-        it('should return true for a registered address', async () => {
-            const registeredAddress =
-                '0x1234567890123456789012345678901234567890';
-            await authentication.register(registeredAddress);
-            const result = await authentication.verifyUserIsRegistered(
-                registeredAddress
-            );
-            expect(result).to.equal(true);
-        });
-
-        it('should return false for an unregistered address', async () => {
-            const unregisteredAddress =
-                '0x0987654321098765432109876543210987654321';
-            const result = await authentication.verifyUserIsRegistered(
-                unregisteredAddress
-            );
-            expect(result).to.equal(false);
-        });
+    it('should return false for an unregistered address', async () => {
+      const unregisteredAddress = '0x0987654321098765432109876543210987654321';
+      const result = await authentication.verifyUserIsRegistered(
+        unregisteredAddress
+      );
+      expect(result).to.equal(false);
     });
+  });
 });
