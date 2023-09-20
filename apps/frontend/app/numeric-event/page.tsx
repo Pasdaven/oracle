699c1708067e274b7c1a11a70770aa6eafb97975
Yen Cheng <92412722+ridemountainpig@users.noreply.github.com>
2023-09-20 22:02:15 +0800

    feat: numeric problem voting event browsing page (#224)
    * feat: add numeric event browsing page
    
    * style: formatting
    
    ---------
    
    Co-authored-by: David Ho <davidho0403@gmail.com>

--- apps/frontend/app/numeric-event/page.tsx
+++ apps/frontend/app/numeric-event/page.tsx
@@ -2,10 +2,10 @@
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { getStringEvent as fetchStringEvent } from '@/lib/oracle';
+import { getNumericEvent as fetchNumericEvent } from '@/lib/oracle';
 import { checkMetamaskLogin } from '@/lib/metamask';
 
-export default function StringEventPage() {
+export default function NumberEventPage() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(true);
 
@@ -27,7 +27,7 @@ export default function StringEventPage() {
   const fetchData = async () => {
     const metamaskAccount = window.ethereum.selectedAddress;
     try {
-      const eventContent = await fetchStringEvent(metamaskAccount || '');
+      const eventContent = await fetchNumericEvent(metamaskAccount || '');
       setEvent(eventContent);
     } catch (error) {
       console.error('發生錯誤：', error);
@@ -39,14 +39,14 @@ export default function StringEventPage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="flex justify-center w-full">
-        <h1 className="text-4xl font-bold">String Event</h1>
+        <h1 className="text-4xl font-bold">Numeric Event</h1>
       </div>
       <div className="flex justify-center w-full py-5">
         <button
           className="px-4 py-2 rounded-md bg-purple-600 cursor-pointer hover:bg-purple-500 text-xl font-semibold duration-100 text-white"
           onClick={fetchData}
         >
-          Get String Event
+          Get Numeric Event
         </button>
       </div>
       <div className="flex justify-center w-full py-5">
