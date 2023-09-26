d2319104736b2df4d9768b6351f7d9a91650fb41
pascal <a306090603@gmail.com>
2023-09-27 01:54:26 +0800

    feat: add reputation scores mechanism

--- apps/blockchain/contracts/Authentication.sol
+++ apps/blockchain/contracts/Authentication.sol
@@ -1,32 +1,71 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import './DataManager.sol';
+
 contract Authentication {
+  // Events
   event Register(bool isRegistered);
 
-  address[] public users;
+  // Variables
+  DataManager private dataManager;
+  mapping(address => uint256) public reputationScores;
 
-  function register(address _walletAddress) public {
-    if (!verifyUserIsRegistered(_walletAddress)) {
-      users.push(_walletAddress);
-      emit Register(true);
-    } else {
-      emit Register(false);
-    }
+  // Constructor
+  constructor(address _dataManagerAddr) {
+    dataManager = DataManager(_dataManagerAddr);
   }
 
-  function getUsers() external view returns (address[] memory) {
-    return users;
+  // Functions
+  function getReputationScoresByAddress(
+    address _walletAddress
+  ) external view returns (uint256) {
+    return reputationScores[_walletAddress];
+  }
+
+  function register(address _walletAddress) public returns (bool) {
+    if (!verifyUserIsRegistered(_walletAddress)) {
+      reputationScores[_walletAddress] = 100;
+      dataManager.setUser(_walletAddress);
+      emit Register(true);
+      return true;
+    } else {
+      emit Register(false);
+      return false;
+    }
   }
 
   function verifyUserIsRegistered(
     address _walletAddress
   ) public view returns (bool) {
-    for (uint256 i = 0; i < users.length; i++) {
-      if (users[i] == _walletAddress) {
+    address[] memory _users = dataManager.getUsers();
+    for (uint256 i = 0; i < _users.length; i++) {
+      if (_users[i] == _walletAddress) {
         return true;
       }
     }
     return false;
+  }
+
+  function adjustCredibilityScore(
+    address _walletAddress,
+    int256 _scoreChange
+  ) public {
+    require(verifyUserIsRegistered(_walletAddress), 'User does not registered');
+    uint256 absScoreChange = uint256(
+      _scoreChange > 0 ? _scoreChange : -_scoreChange
+    );
+    if (_scoreChange > 0) {
+      reputationScores[_walletAddress] = reputationScores[_walletAddress] +
+        absScoreChange >
+        100
+        ? 100
+        : reputationScores[_walletAddress] + absScoreChange;
+    } else {
+      reputationScores[_walletAddress] = reputationScores[_walletAddress] >=
+        absScoreChange
+        ? reputationScores[_walletAddress] - absScoreChange
+        : 0;
+    }
   }
 }
