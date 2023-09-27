e0ea16479396baa6c9643896fb161c3e7f10d887
ridemountainpig <lin0905580802@gmail.com>
2023-09-27 16:55:07 +0800

    style: format code

--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -1,66 +1,61 @@
 'use client';
 
-import { Metadata } from "next"
+import { Metadata } from 'next';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { checkMetamaskLogin } from '@/lib/metamask';
 
-import { BrainCircuit, Rocket, DollarSign, Banknote } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { BrainCircuit, Rocket, DollarSign, Banknote } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import {
-    Tabs,
-    TabsContent,
-    TabsList,
-    TabsTrigger,
-} from "@/components/ui/tabs"
-import { MainNav } from "@/components/main-nav"
-import { UserNav } from "@/components/user-nav"
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { MainNav } from '@/components/main-nav';
+import { UserNav } from '@/components/user-nav';
 
 export default function DashboardPage() {
-    const router = useRouter();
-    const [walletAddress, setWalletAddress] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
+  const router = useRouter();
+  const [walletAddress, setWalletAddress] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const checkLogin = async () => {
-            const isLoggedIn = await checkMetamaskLogin();
-            if (!isLoggedIn) {
-                router.push('/login'); // redirect to login page
-            } else {
-                setWalletAddress(window.ethereum.selectedAddress || '');
-                setIsLoading(false); // Metamask login is complete
-            }
-        };
+  useEffect(() => {
+    const checkLogin = async () => {
+      const isLoggedIn = await checkMetamaskLogin();
+      if (!isLoggedIn) {
+        router.push('/login'); // redirect to login page
+      } else {
+        setWalletAddress(window.ethereum.selectedAddress || '');
+        setIsLoading(false); // Metamask login is complete
+      }
+    };
 
-        checkLogin();
-    }, [router]);
+    checkLogin();
+  }, [router]);
 
-    if (isLoading) return null; // wait for Metamask login to complete
+  if (isLoading) return null; // wait for Metamask login to complete
 
-    return (
-        <>
-            <div className="flex-col md:flex">
-                <div className="border-b">
-                    <div className="flex h-16 items-center px-4">
-                        <MainNav className="mx-6" />
-                        <div className="ml-auto flex items-center space-x-4">
-                            <UserNav walletAddress={walletAddress} />
-                        </div>
-                    </div>
-                </div>
-                <div className="flex-1 space-y-4 p-8 pt-6">
-                    <div className="flex items-center justify-between space-y-2">
-                        <h2 className="text-3xl font-bold tracking-wide">Overview</h2>
-                    </div>
-                    <Tabs defaultValue="overview" className="space-y-4">
-                        {/* <TabsList>
+  return (
+    <>
+      <div className="flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <UserNav walletAddress={walletAddress} />
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <div className="flex items-center justify-between space-y-2">
+            <h2 className="text-3xl font-bold tracking-wide">Overview</h2>
+          </div>
+          <Tabs defaultValue="overview" className="space-y-4">
+            {/* <TabsList>
                             <TabsTrigger value="overview">Overview</TabsTrigger>
                             <TabsTrigger value="analytics" >
                                 Analytics
@@ -72,76 +67,78 @@ export default function DashboardPage() {
                                 Notifications
                             </TabsTrigger>
                         </TabsList> */}
-                        <TabsContent value="overview" className="space-y-4">
-                            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                                <Card>
-                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                        <CardTitle className="text-base font-medium">
-                                            Answer Question
-                                        </CardTitle>
-                                        <BrainCircuit />
-                                    </CardHeader>
-                                    <CardContent>
-                                        <div className="text-3xl font-bold">10</div>
-                                    </CardContent>
-                                </Card>
-                                <Card>
-                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                        <CardTitle className="text-base font-medium">Reputation Points</CardTitle>
-                                        <Rocket />
-                                    </CardHeader>
-                                    <CardContent>
-                                        <div className="text-3xl font-bold">100</div>
-                                    </CardContent>
-                                </Card>
-                                <Card>
-                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                        <CardTitle className="text-base font-medium">
-                                            Total Revenue
-                                        </CardTitle>
-                                        <DollarSign />
-                                    </CardHeader>
-                                    <CardContent>
-                                        <div className="text-3xl font-bold">40 <span className="text-xs">ETH</span></div>
-                                    </CardContent>
-                                </Card>
-                                <Card>
-                                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                                        <CardTitle className="text-base font-medium">
-                                            Pledge
-                                        </CardTitle>
-                                        <Banknote />
-                                    </CardHeader>
-                                    <CardContent>
-                                        <div className="text-3xl font-bold">25 <span className="text-xs">ETH</span></div>
-                                    </CardContent>
-                                </Card>
-                            </div>
-                            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                                <Card className="col-span-4">
-                                    <CardHeader>
-                                        <CardTitle>Recent Answer Question</CardTitle>
-                                    </CardHeader>
-                                    <CardContent className="pl-2">
-
-                                    </CardContent>
-                                </Card>
-                                <Card className="col-span-3">
-                                    <CardHeader>
-                                        <CardTitle>Transaction History</CardTitle>
-                                        <CardDescription>
-                                            You made 265 sales this month.
-                                        </CardDescription>
-                                    </CardHeader>
-                                    <CardContent>
-
-                                    </CardContent>
-                                </Card>
-                            </div>
-                        </TabsContent>
-                    </Tabs>
-                </div>
-            </div>
-        </>
-    )
+            <TabsContent value="overview" className="space-y-4">
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-base font-medium">
+                      Answer Question
+                    </CardTitle>
+                    <BrainCircuit />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-3xl font-bold">10</div>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-base font-medium">
+                      Reputation Points
+                    </CardTitle>
+                    <Rocket />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-3xl font-bold">100</div>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-base font-medium">
+                      Total Revenue
+                    </CardTitle>
+                    <DollarSign />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-3xl font-bold">
+                      40 <span className="text-xs">ETH</span>
+                    </div>
+                  </CardContent>
+                </Card>
+                <Card>
+                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                    <CardTitle className="text-base font-medium">
+                      Pledge
+                    </CardTitle>
+                    <Banknote />
+                  </CardHeader>
+                  <CardContent>
+                    <div className="text-3xl font-bold">
+                      25 <span className="text-xs">ETH</span>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+                <Card className="col-span-4">
+                  <CardHeader>
+                    <CardTitle>Recent Answer Question</CardTitle>
+                  </CardHeader>
+                  <CardContent className="pl-2"></CardContent>
+                </Card>
+                <Card className="col-span-3">
+                  <CardHeader>
+                    <CardTitle>Transaction History</CardTitle>
+                    <CardDescription>
+                      You made 265 sales this month.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent></CardContent>
+                </Card>
+              </div>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </>
+  );
 }
--- apps/frontend/components/main-nav.tsx
+++ apps/frontend/components/main-nav.tsx
@@ -1,49 +1,47 @@
-import Link from "next/link"
+import Link from 'next/link';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 export function MainNav({
-    className,
-    ...props
+  className,
+  ...props
 }: React.HTMLAttributes<HTMLElement>) {
-    return (
-        <nav
-            className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-            {...props}
-        >
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="h-6 w-6"
-            >
-                <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg>
-            <div className="text-xl font-extrabold">
-                Oracle
-            </div>
-            <Link
-                href="/dashboard"
-                className="text-sm font-medium transition-colors hover:text-primary"
-            >
-                Overview
-            </Link>
-            <Link
-                href="/numeric-event"
-                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-            >
-                Numeric Question
-            </Link>
-            <Link
-                href="/string-event"
-                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
-            >
-                String Question
-            </Link>
-        </nav>
-    )
+  return (
+    <nav
+      className={cn('flex items-center space-x-4 lg:space-x-6', className)}
+      {...props}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-6 w-6"
+      >
+        <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+      </svg>
+      <div className="text-xl font-extrabold">Oracle</div>
+      <Link
+        href="/dashboard"
+        className="text-sm font-medium transition-colors hover:text-primary"
+      >
+        Overview
+      </Link>
+      <Link
+        href="/numeric-event"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        Numeric Question
+      </Link>
+      <Link
+        href="/string-event"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
+        String Question
+      </Link>
+    </nav>
+  );
 }
