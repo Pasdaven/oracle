bb29f49f4f77f6d6e7ab0ae174f6ec8ecb43dd9f
ridemountainpig <lin0905580802@gmail.com>
2023-09-29 00:38:50 +0800

    feat: add route check

--- apps/frontend/app/dashboard/page.tsx
+++ apps/frontend/app/dashboard/page.tsx
@@ -46,14 +46,14 @@ export default function DashboardPage() {
         <div className="border-b">
           <div className="flex h-16 items-center px-4">
             <MainNav className="mx-6" />
-            <div className="ml-auto flex items-center space-x-4">
+            <div className="ml-auto flex items-center space-x-4 mx-6">
               <UserNav walletAddress={walletAddress} />
             </div>
           </div>
         </div>
-        <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex-1 space-y-4 px-10 py-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-wide">Overview</h2>
+            <h2 className="text-3xl font-semibold tracking-wide">Overview</h2>
           </div>
           <Tabs defaultValue="overview" className="space-y-4">
             {/* <TabsList>
