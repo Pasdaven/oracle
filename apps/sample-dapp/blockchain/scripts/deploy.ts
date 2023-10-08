0506a87e56b01d8252ca4fd7e6a0accf4cf81aba
David Ho <davidho0403@gmail.com>
2023-10-09 02:18:34 +0800

    feat: implement sample dapp calling oracle

--- apps/sample-dapp/blockchain/scripts/deploy.ts
+++ apps/sample-dapp/blockchain/scripts/deploy.ts
@@ -4,9 +4,6 @@ async function main() {
   const CallbackContract = await ethers.getContractFactory('Callback');
   const callbackContract = await CallbackContract.deploy();
   console.log('CallbackContract deployed to:', callbackContract.address);
-  const AppContract = await ethers.getContractFactory('App');
-  const appContract = await AppContract.deploy();
-  console.log('AppContract deployed to:', appContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
